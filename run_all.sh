d8ce10471db5d777a6435467bfd1a333a605d9bd
jmfveneroso <jmfveneroso@gmail.com>
2019-02-14 13:54:01 -0200

    LDA notebook.

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,16 @@
 user="$(whoami)"
 if [[ $user == "root" ]]; then
-  for filename in ./configs/*.json; do
+  # for filename in ./configs/*.json; do
+  while true; do
+    ls -l ./configs/test_*.json &>/dev/null
+    if [ $? -ne 0 ]; then
+      break
+    fi
+
+    filename=$(ls -l ./configs/test_*.json | awk '{if (NR == 1) print $9}')
     f=${filename%.*}
     f=${f:10}
-    echo "Running $f..."
+    echo "Running $filename ($f)..."
     mkdir -p results/$f
     python run.py train -j $filename > results/$f/${f}.log
     ./eval.sh >> results/$f/${f}.log
@@ -19,5 +26,7 @@ if [[ $user == "root" ]]; then
     if [ "$(ls -A results/score)" ]; then
       mv results/score/* results/$f/
     fi
+    mv ./configs/$f.json ./configs/$f.json.done
   done
+  echo 'Done!!!'
 fi
